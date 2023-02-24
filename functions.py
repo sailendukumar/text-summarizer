dc6e09c8a41b367c89fb2d5190015fe5205e23e0
Sailendu <32447348+sailendukumar@users.noreply.github.com>
2023-02-24 15:21:22 +0530

    Add files via upload

--- functions.py
+++ functions.py
@@ -4,12 +4,11 @@ import streamlit as st
 def summarize(prompt):
     augmented_prompt = f"summarize this text: {prompt}"
     try:
-      st.session_state["summary"] = openai.Completion.create(
-        model="text-davinci-003",
-        prompt=augmented_prompt,
-        temperature=.5,
-        max_tokens=1000,
-      )["choices"][0]["text"]
-      
-     except:
-       st.write('There was an error =(')
+        st.session_state["summary"] = openai.Completion.create(
+            model="text-davinci-003",
+            prompt=augmented_prompt,
+            temperature=.5,
+            max_tokens=1000,
+        )["choices"][0]["text"]
+    except:
+        st.write('There was an error =(')
